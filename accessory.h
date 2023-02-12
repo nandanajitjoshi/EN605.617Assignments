a6a5d966875bb4d0edc61205de8ad56eaacce6f6
nandanajitjoshi <41496619+nandanajitjoshi@users.noreply.github.com>
2023-02-11 17:00:58 -0800

    Delete accessory.h

--- accessory.h
+++
@@ -1,9 +0,0 @@
-/*Contains the headers for all accesory functions to the main module*/
-
-/* Function : printToConsole
-*  Takes the input and output arrays and prints them to the console
-* inputArray1, inputArray2, addArray, subArray, mulArray, modArray : input arrays
-* totalThreads: Number of elements to be pronted
-*/
-
-void printToConsole( int* , int* , int* , int* , int* , int* , int ); 
